7718939d36c8601ff43e9b37c162b3e6b8aa8a65
Chi Shen <shenchi710@gmail.com>
2016-10-29 19:01:26 -0400

    saber

---
+++ 12_Meter_Light_Saber/Assets/Scripts/LightSaber.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(LineRenderer))]
+public class LightSaber : MonoBehaviour
+{
+    public float length = 12;
+    public float angle = 0;
+    public float offset = 0;
+    
+    public float upperAngle = Mathf.PI / 4;
+    public float lowerAngle = Mathf.PI / 4;
+
+    public float prepareDuration = 0.1f;
+    public float swingDuration = 0.2f;
+    public float frozenDuration = 0.1f;
+
+    public bool Swinging { get; private set; }
+
+    private LineRenderer line;
+
+    void OnEnable()
+    {
+        line = GetComponent<LineRenderer>();
+        line.enabled = false;
+    }
+    
+    private float startAngle, endAngle, swingTime;
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Swinging)
+        {
+            swingTime += Time.fixedDeltaTime;
+
+            if (swingTime < prepareDuration)
+            {
+                angle = startAngle;
+            }
+            else if (swingTime < prepareDuration + swingDuration)
+            {
+                float t = swingTime / swingDuration;
+                angle = Mathf.Lerp(startAngle, endAngle, t);
+            }
+            else if (swingTime < prepareDuration + swingDuration + frozenDuration)
+            {
+                angle = endAngle;
+            }
+            else
+            {
+                Swinging = false;
+            }
+        }
+
+        line.enabled = Swinging;
+        if (Swinging)
+        {
+            Vector3 start = transform.position;
+            Vector3 tip = transform.position;
+
+            start.x = start.x + Mathf.Sin(angle) * offset;
+            start.y = start.y + Mathf.Cos(angle) * offset;
+
+            line.SetPosition(0, start);
+
+            tip.x = tip.x + Mathf.Sin(angle) * (offset + length);
+            tip.y = tip.y + Mathf.Cos(angle) * (offset + length);
+
+            line.SetPosition(1, tip);
+        }
+    }
+
+    public void Swing(float aimingAngle)
+    {
+        if (aimingAngle > 0.0f)
+        {
+            startAngle = aimingAngle - upperAngle;
+            endAngle = aimingAngle + lowerAngle;
+        }
+        else
+        {
+            startAngle = aimingAngle + upperAngle;
+            endAngle = aimingAngle - lowerAngle;
+        }
+        swingTime = 0.0f;
+        Swinging = true;
+    }
+}
---
+++ 12_Meter_Light_Saber/Assets/Scripts/LightSaberController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSaberController : MonoBehaviour
+{
+    public LightSaber[] sabers;
+
+    private Camera cam;
+
+    private Vector2 aimingDir = Vector2.up;
+    private float aimingAngle;
+
+    
+    private float lastTrigger = 0.0f;
+    void Update()
+    {
+        Vector2 stickDir = new Vector2(Input.GetAxis("RStickX"), Input.GetAxis("RStickY"));
+
+        if (stickDir.magnitude > 0.0f)
+        {
+            aimingDir = stickDir;
+        }
+        else
+        {
+            if (!cam)
+                cam = FindObjectOfType<Camera>();
+
+            aimingDir = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        }
+
+        if (aimingDir.magnitude > 0.0f)
+        {
+            aimingAngle = Mathf.Deg2Rad * Vector2.Angle(Vector2.up, aimingDir.normalized);
+            if (aimingDir.x < 0) aimingAngle = -aimingAngle;
+        }
+
+        bool swinging = false;
+        for (int i = 0; i < sabers.Length; i++)
+        {
+            swinging = swinging || sabers[i].Swinging;
+        }
+
+        if (!swinging && (Input.GetButtonDown("Fire1") || Input.GetAxisRaw("Fire1") - lastTrigger > 0.2f))
+        {
+            for (int i = 0; i < sabers.Length; i++)
+            {
+                sabers[i].Swing(aimingAngle);
+            }
+        }
+
+        lastTrigger = Input.GetAxisRaw("Fire1");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 dir = aimingDir;
+        Gizmos.DrawLine(transform.position, transform.position + dir.normalized);
+    }
+}
